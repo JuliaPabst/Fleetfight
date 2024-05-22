9027485f8d2e0b54d4d60a506e064ab4628a47aa
Julia Pabst <ju.pabst@gmail.com>
2024-05-22 15:12:23 +0200

    add Player class

---
+++ project/Player.h
@@ -0,0 +1,15 @@
+#ifndef PROJECT_PLAYER_H
+#define PROJECT_PLAYER_H
+#include "Ships.h"
+
+
+class Player {
+public:
+private:
+    vector<Ship> ships_;
+    int playerNumber_
+
+};
+
+
+#endif //PROJECT_PLAYER_H
--- project/main.cpp
+++ project/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "Game.h"
 
 int main() {
+    Game game();
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
